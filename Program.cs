fe385faf9d6b19f4a7714026f442d70ba96c8df8
nastyaduko <kozelskaya_anast@mail.ru>
2023-02-13 16:33:38 +0300

    Program first version

--- Program.cs
+++ Program.cs
@@ -1,2 +1,47 @@
-﻿        // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = { "кот", "волк", "сок", "муха", "бук", "куб" };
+int size = NumberOfShortElements(array);
+string[] newArray = new string[size];
+newArray = FillArray(array, newArray);
+Print(array);
+Print(newArray);
+
+
+// ---------------Methods---------------------
+
+int NumberOfShortElements(string[] arr)
+{
+    int count = 0;
+    foreach (string el in arr)
+    {
+        if (el.Length < 4) count++;
+    }
+    return count;
+}
+
+string[] FillArray(string[] massiv, string[] collection)
+{
+    // string[] collection = new string[num];
+    for (int i = 0; i < massiv.Length;)
+    {
+        for (int j = 0; j < collection.Length;)
+        {
+            if (massiv[i].Length < 4)
+            {
+                collection[j] = massiv[i];
+                i++; j++;
+            }
+            else i++;
+        }
+    }
+    return newArray;
+}
+
+void Print(string[] col)
+{
+    for (int i = 0; i < col.Length; i++)
+    {
+        Console.Write($"{col[i] + " "}");
+    }
+    Console.WriteLine();
+}
+
